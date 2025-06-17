9233ca335fd45a94a9116b2e87176fb367f8b746
pedro.tsouza <pedro.tsouza@solutis.com.br>
2025-06-17 15:06:31 -0300

    Ajustes das dtos de curso

--- curso-service/src/main/java/com/cursos/dto/curso/CursoResponseMatricula.java
+++ curso-service/src/main/java/com/cursos/dto/curso/CursoResponseMatricula.java
@@ -1,4 +1,19 @@
 package com.cursos.dto.curso;
 
-public record CursoResponseMatricula(Long id, String titulo, String descricao, String categoria, Double preco, Double nota) {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CursoResponseMatricula {
+    private Long id;
+    private String titulo;
+    String descricao;
+    String categoria;
+    Double preco;
+    Double nota;
 }
