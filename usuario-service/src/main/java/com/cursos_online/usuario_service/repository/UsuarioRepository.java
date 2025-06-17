73c3af7e4b77cf0818b6e2c73a926999a0a8c7a8
FABIO THIAGO DE AZEVEDO <fabio.azevedo@solutis.net.br>
2025-06-17 14:10:04 -0300

    Implementando JWT em Usuario

--- usuario-service/src/main/java/com/cursos_online/usuario_service/repository/UsuarioRepository.java
+++ usuario-service/src/main/java/com/cursos_online/usuario_service/repository/UsuarioRepository.java
@@ -12,4 +12,6 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Boolean existsByEmail(String email);
 
     Optional<Usuario> findByEmailEqualsIgnoreCase(String email);
+
+    Optional<Usuario> findByEmail(String username);
 }
