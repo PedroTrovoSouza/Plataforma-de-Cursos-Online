139c11043948bdd5ee9529c314ca30d79348d343
Pedro Trovo <pedro.tsouza@solutis.com.br>
2025-06-17 14:02:22 -0300

    Update CursoService.java

--- curso-service/src/main/java/com/cursos/service/CursoService.java
+++ curso-service/src/main/java/com/cursos/service/CursoService.java
@@ -1,6 +1,5 @@
 package com.cursos.service;
 
-import com.cursos.entity.Avaliacao;
 import com.cursos.entity.Curso;
 import com.cursos.exception.CursoConflitoException;
 import com.cursos.exception.CursoNaoEncontradoException;
@@ -21,11 +20,9 @@ public class CursoService {
     }
 
     public Curso cadastrarNovoCurso(Curso cursoParaCadastrar){
-        if (cursoRepository.existsByTituloContainingIgnoreCaseOrDescricaoContainingIgnoreCase
-                (cursoParaCadastrar.getTitulo(), cursoParaCadastrar.getDescricao())){
+        if (cursoRepository.existsByTituloOrDescricaoAllIgnoreCase(cursoParaCadastrar.getTitulo(), cursoParaCadastrar.getDescricao())){
             throw new CursoConflitoException("Curso com Titulo ou Descrição ja cadastrado!");
         }
-        cursoParaCadastrar.setNota(0.0);
         return cursoRepository.save(cursoParaCadastrar);
     }
 
@@ -38,30 +35,30 @@ public class CursoService {
         if(cursoRepository.existsByTitulo(novoTItulo)){
             throw new CursoConflitoException("Curso com Titulo identico ja cadastrado!");
         }
-        Curso cursoParaAtualizar = buscarCursoPorId(id);
-        cursoParaAtualizar.setTitulo(novoTItulo);
-        return cursoRepository.save(cursoParaAtualizar);
+        Curso cursoAtualizado = cursoRepository.save(buscarCursoPorId(id));
+        cursoAtualizado.setTitulo(novoTItulo);
+        return cursoAtualizado;
     }
 
     public Curso atualizarDescricaoDoCurso(Long id, String novaDescricao){
         if(cursoRepository.existsByDescricao(novaDescricao)){
             throw new CursoConflitoException("Curso com Descrição identico ja cadastrado!");
         }
-        Curso cursoParaAtualizar = buscarCursoPorId(id);
-        cursoParaAtualizar.setDescricao(novaDescricao);
-        return cursoRepository.save(cursoParaAtualizar);
+        Curso cursoAtualizado = cursoRepository.save(buscarCursoPorId(id));
+        cursoAtualizado.setDescricao(novaDescricao);
+        return cursoAtualizado;
     }
 
     public Curso atualizarCategoria(Long id, String novaCategoria){
-        Curso cursoParaAtualizar = buscarCursoPorId(id);
-        cursoParaAtualizar.setCategoria(novaCategoria);
-        return cursoRepository.save(cursoParaAtualizar);
+        Curso cursoAtualizado = cursoRepository.save(buscarCursoPorId(id));
+        cursoAtualizado.setCategoria(novaCategoria);
+        return cursoAtualizado;
     }
 
     public Curso atualizarPreco(Long id, Double novoPreco){
-        Curso cursoParaAtualizar = buscarCursoPorId(id);
-        cursoParaAtualizar.setPreco(novoPreco);
-        return cursoRepository.save(cursoParaAtualizar);
+        Curso cursoAtualizado = cursoRepository.save(buscarCursoPorId(id));
+        cursoAtualizado.setPreco(novoPreco);
+        return cursoAtualizado;
     }
 
     public void deletarCursoPorId(Long id){
@@ -69,13 +66,14 @@ public class CursoService {
         cursoRepository.delete(cursoParaDeletar);
     }
 
-    public List<Curso> listarPorCategoria(String categoria) {
-        return cursoRepository.findAllByCategoriaContaining(categoria);
+    public void deletarCursoPorTitulo(String titulo){
+        Curso cursoParaDeletar = cursoRepository.findByTitulo(titulo)
+                .orElseThrow(() -> new CursoNaoEncontradoException("Curso com Título não encontrado."));
+        cursoRepository.delete(cursoParaDeletar);
     }
 
-    public void atualizarNotaDoCurso(Long idCurso, List<Avaliacao> avaliacoes) {
-        Curso curso = buscarCursoPorId(idCurso);
-        curso.atualizarNota(avaliacoes);
-        cursoRepository.save(curso);
+    public Curso buscarCursoPorNome(String nome) {
+        return cursoRepository.findByTitulo(nome)
+                .orElseThrow(() -> new CursoNaoEncontradoException("Curso com ID não encontrado!"));
     }
 }
