9b8dc4a6361ba623e1fe8f611660cd2e7c102092
euFernandoAmoriim <fernandoalmeida.mda@gmail.com>
2025-06-23 16:03:20 -0300

    iniciando classes de teste

---
+++ curso-service/src/test/java/com/cursos/controller/CursoControllerTest.java
@@ -0,0 +1,113 @@
+package com.cursos.controller;
+
+import com.cursos.dto.curso.CursoRequestDto;
+import com.cursos.dto.curso.CursoResponseDto;
+import com.cursos.entity.Avaliacao;
+import com.cursos.entity.Curso;
+import com.cursos.mapper.CursoMapper;
+import com.cursos.service.CursoService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+class CursoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private CursoService cursoService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private Curso curso;
+
+    private Avaliacao avaliacao1;
+
+    private Avaliacao avaliacao2;
+
+    private Avaliacao avaliacao3;
+
+    private List<Avaliacao> avaliacoes;
+
+    @BeforeEach
+    void setUp(){
+        avaliacoes = new ArrayList<>();
+        avaliacao1 = new Avaliacao(1L, 5.0, "Curso muito bom eu ganhei 100 reais em uma semana", 1L, "Fernando", curso);
+        avaliacoes.add(avaliacao1);
+        avaliacao2 = new Avaliacao(2L, 2.0, "Nao gostei, ainda nao ganhei nada em 2 meses", 2L, "Paulo", curso);
+        avaliacoes.add(avaliacao2);
+        avaliacao3 = new Avaliacao(3L, 0.5, "Faz 1 ano que eu nao ganho nada", 3L, "Ricardo", curso);
+        avaliacoes.add(avaliacao3);
+        curso = new Curso("Como ficar rico Vendendo cursos online", "Aprenda a como ficar milionario em 3 dias com este curso incrivel", "Dinheiro", 799.90, 3.0, avaliacoes);
+    }
+
+    @Test
+    void deveRetornarCodigo200EListarCurso() throws Exception {
+        Curso cursoCadastrado = cursoService.cadastrarNovoCurso(curso);
+
+        CursoResponseDto cursoResponseDto = CursoMapper.toResponseDto(cursoCadastrado);
+
+        mockMvc.perform(get("/cursos"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].titulo").value(cursoResponseDto.getTitulo()))
+                .andExpect(jsonPath("$[0].descricao").value(cursoResponseDto.getDescricao()))
+                .andExpect(jsonPath("$[0].nota").value(cursoResponseDto.getNota()));
+    }
+
+    @Test
+    void deveRetornarCodigo204EListaVazia() throws Exception {
+        mockMvc.perform(get("/cursos"))
+                .andExpect(status().isNoContent())
+                .andExpect(content().string(""));
+    }
+
+    @Test
+    void deveCadastrarNovoCursoComSucesso() throws Exception {
+        CursoRequestDto cursoRequestDto = new CursoRequestDto(curso.getTitulo(), curso.getDescricao(), curso.getCategoria(), curso.getPreco());
+
+        mockMvc.perform(post("/cursos").contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(cursoRequestDto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.titulo").value(curso.getTitulo()));
+    }
+
+    @Test
+    void deveBuscarCursoPorIdInformadoERetornarCodigo200() throws Exception {
+    Curso cursoCadastrado = cursoService.cadastrarNovoCurso(curso);
+    CursoRequestDto cursoRequestDto = new CursoRequestDto(curso.getTitulo(), curso.getDescricao(), curso.getCategoria(), curso.getPreco());
+
+        mockMvc.perform(get("/cursos/id/{id}", cursoCadastrado.getId()))
+                .andExpect(jsonPath("$.titulo").value(curso.getTitulo()));
+    }
+
+    @Test
+    void deveAtualizarDescricaoDoCursoComIdInformado() throws Exception {
+        Curso cursoCadastrado = cursoService.cadastrarNovoCurso(curso);
+        String novaDescricao = "Nova Descricao";
+
+        mockMvc.perform(patch("/cursos/descricao/{id}", cursoCadastrado.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(novaDescricao))
+                .andExpect(jsonPath("$.descricao").value(novaDescricao));
+    }
+
+}
